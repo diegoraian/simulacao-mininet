aab50f872fb7e428e08ef04c5ad0d2e0dd7971dc
Diego Raian <diegoraian@hotmail.com>
2018-07-14 03:07:31 -0300

    Ajustes server

--- Server.java
+++ Server.java
@@ -1,4 +1,3 @@
-
 import java.net.BindException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -8,34 +7,34 @@ import java.util.Date;
 
 public class Server {
 	private static final Integer PORTA = 6001;
-	private static final String IP = "192.168.1.15";
 
 	public static void main(String[] args) {
-		System.out.println("O servidor NTP está no ar!");
+
 		DatagramSocket serverSocketUDP = null;
+		System.out.println("Sincronizador funcionando");
 		while (true) {
 			try {
-					Thread.currentThread().sleep(1000);
-					byte[] buffer = new byte[12];
-					DatagramPacket pacote = new DatagramPacket(buffer, 12);
-					serverSocketUDP.receive(pacote);
-					pacote.getAddress();
-					String ipServidor = new String(pacote.getData(),StandardCharsets.UTF_8);
-					System.out.println("Pacote recebido " + ipServidor);
-					if(ipServidor != null && !ipServidor.isEmpty()) {
-						
-						Date date = new Date();
-						date.getTime();
-						InetAddress ip = InetAddress.getByName(ipServidor);
-						String mensagemEnvio = date.toString();
-						byte[] bufferEnvio = mensagemEnvio.getBytes(StandardCharsets.UTF_8);
-						System.out.println(bufferEnvio.length);
-						DatagramPacket pacoteEnvio = new DatagramPacket(bufferEnvio, bufferEnvio.length, ip, PORTA);
-						serverSocketUDP.send(pacoteEnvio);
-						System.out.println("Pacote enviado " + mensagemEnvio);
-					}
+				serverSocketUDP =  new DatagramSocket(PORTA);
+				Thread.currentThread().sleep(1000);
+				byte[] bufferConsulta = new byte[12];
+				DatagramPacket pacote = new DatagramPacket(bufferConsulta, 12);
+				serverSocketUDP.receive(pacote);
+				String ipServidor = new String(pacote.getData(),StandardCharsets.UTF_8);
+				System.out.println("Pacote recebido " + ipServidor);
+				if(ipServidor != null && !ipServidor.isEmpty()) {
+					
+					Date date = new Date();
+					date.getTime();
+					InetAddress ip = InetAddress.getByName(ipServidor);
+					String mensagem = date.toString();
+					byte[] buffer = mensagem.getBytes(StandardCharsets.UTF_8);
+					System.out.println(buffer.length);
+					DatagramPacket pacoteEnvio = new DatagramPacket(buffer, buffer.length, ip, PORTA);
+					serverSocketUDP.send(pacoteEnvio);
+					System.out.println("Pacote enviado " + mensagem);
 					serverSocketUDP.close();
-	
+				}
+				serverSocketUDP.close();
 			} catch(BindException be) {
 				System.out.println(be.getMessage());
 			}catch (Exception e) {
@@ -44,7 +43,8 @@ public class Server {
 				}
 				e.printStackTrace();
 			}
+
 		}
 	}
-
+	
 }
